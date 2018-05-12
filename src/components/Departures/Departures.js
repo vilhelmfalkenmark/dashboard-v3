f47fa47e8d7bfbdfcf545f3b1783e4f5bbc08dc6
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2018-05-12 18:08:30 +0200

    first manual query working

--- src/components/Departures/Departures.js
+++ src/components/Departures/Departures.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ApolloConsumer } from "react-apollo";
 
 import WithCss from "layout/WithCss";
 import Input from "components/Input";
@@ -10,53 +11,56 @@ import DeparturesTypes from "./Departures.propTypes";
 class Departures extends Component {
   constructor() {
     super();
-
-    this.state = {
-      searchStation: ""
-    };
-
-    this.searchStationByName = this.searchStationByName.bind(this);
+    this.state = { searchStation: "", stationsByName: [] };
   }
-
-  searchStationByName() {
-    // this.props.client.query({
-    //   query: SEARCH_STATION_BY_NAME,
-    //   variables: {
-    //     stationNameQuery: "Odenplan"
-    //   }
-    // });
-  }
+  onStationsFetched = stationsByName => this.setState({ stationsByName });
 
   render() {
-    console.log(this.props, " <-- this.props");
-    const { data } = this.props;
+    // console.log(this.props, " <-- this.props");
+    // console.log(this.state, " <-- this.state");
+    // const { data } = this.props;
+    const { stationsByName } = this.state;
 
     return (
-      <div className={s({ container: true })}>
-        <h2 className={s({ heading: true })}>Avgångar</h2>
-        <Input
-          inputLabel="Sök station"
-          inputPlaceholder="Exempelvis odenplan"
-          inputName="searchStation"
-          inputDisabled={false}
-          inputValue={this.state.searchStation}
-          inputOnChange={value =>
-            this.setState({
-              searchStation: value
-            })
-          }
-        />
-        <button onClick={this.searchStationByName}>Sök stationer</button>
-        {!data.loading && data.stationsByName ? (
-          <ul>
-            {data.stationsByName.map((station, i) => (
-              <li key={i}>{station.name}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>laddar</p>
+      <ApolloConsumer>
+        {client => (
+          <div className={s({ container: true })}>
+            <h2 className={s({ heading: true })}>Avgångar</h2>
+            <Input
+              inputLabel="Sök station"
+              inputPlaceholder="Exempelvis odenplan"
+              inputName="searchStation"
+              inputDisabled={false}
+              inputValue={this.state.searchStation}
+              inputOnChange={value =>
+                this.setState({
+                  searchStation: value
+                })
+              }
+            />
+            <button
+              onClick={async () => {
+                const { data } = await client.query({
+                  query: SEARCH_STATION_BY_NAME,
+                  variables: { stationName: this.state.searchStation }
+                });
+                this.onStationsFetched(data.stationsByName);
+              }}
+            >
+              Sök stationer
+            </button>
+            {stationsByName ? (
+              <ul>
+                {stationsByName.map((station, i) => (
+                  <li key={i}>{station.name}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>laddar</p>
+            )}
+          </div>
         )}
-      </div>
+      </ApolloConsumer>
     );
   }
 }
--- src/components/Departures/Departures.schema.js
+++ src/components/Departures/Departures.schema.js
@@ -3,23 +3,21 @@ import gql from "graphql-tag";
 import Departures from "./Departures";
 
 export const SEARCH_STATION_BY_NAME = gql`
-  query searchStationByName($stationNameQuery: String!) {
-    stationsByName: searchStationByName(
-      params: { stationName: $stationNameQuery }
-    ) {
+  query searchStationByName($stationName: String!) {
+    stationsByName: searchStationByName(params: { stationName: $stationName }) {
       name: Name
       id: SiteId
     }
   }
 `;
 
-export default compose(
-  withApollo,
-  graphql(SEARCH_STATION_BY_NAME, {
-    options: () => ({
-      variables: {
-        stationNameQuery: "centralen"
-      }
-    })
-  })
-)(Departures);
+// export default compose(
+//   withApollo,
+//   graphql(SEARCH_STATION_BY_NAME, {
+//     options: () => ({
+//       variables: {
+//         stationName: "centralen"
+//       }
+//     })
+//   })
+// )(Departures);
