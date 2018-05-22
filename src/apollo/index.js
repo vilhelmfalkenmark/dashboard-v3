2ea8a9c06138297af47b834472865bdf397bc9f9
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2018-05-22 21:36:44 +0200

    should maybe work with boost

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,15 +1,16 @@
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import ApolloClient from "apollo-client";
 
 const API_END_POINT = process.env.API_END_POINT;
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: createHttpLink({
+  link: new HttpLink({
     uri: `${API_END_POINT}/api`,
     useGETForQueries: true
   }),
-  cache: new InMemoryCache()
+  cache
 });
 
 export default client;
