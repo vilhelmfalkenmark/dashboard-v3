8f5a4ce4d44183f7ec0759ef1cc209dbd06088a2
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2018-05-22 21:16:40 +0200

    cleared heroku NODE cache

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,6 +1,6 @@
-import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
+import ApolloClient from "apollo-client";
 
 const API_END_POINT = process.env.API_END_POINT;
 
