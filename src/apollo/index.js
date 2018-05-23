a2614ad9ae36bd52a9d39c27fe0ee7f5c92e3022
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2018-05-23 09:32:08 +0200

    deleted yarn.lock

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,16 +1,16 @@
-// import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
-import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 const API_END_POINT = process.env.API_END_POINT || "http://localhost:5000";
-// const cache = new InMemoryCache();
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: new HttpLink({
     uri: `${API_END_POINT}/api`,
     useGETForQueries: true
-  })
-  // cache
+  }),
+  cache
 });
 
 export default client;
